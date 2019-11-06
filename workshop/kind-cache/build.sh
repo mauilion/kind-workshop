5eaedd35ecb48261a03fa3a9a79189faffde95a7
Duffie Cooley <cooleyd@vmware.com>
2019-11-06 06:26:31 -0800

    updated a bumch of stuff
    Signed-off-by: Duffie Cooley <cooleyd@vmware.com>

--- workshop/kind-cache/build.sh
+++ workshop/kind-cache/build.sh
@@ -2,13 +2,13 @@
 
 set -xe
 echo "prepulling images"
-#cat images | xargs -I {} docker pull {}
+cat images | xargs -I {} docker pull {}
 
-mkdir -p tars/ tars/mauilion tars/k8s.gcr.io tars/kindest
-#echo "exporting images"
+mkdir -p tars/ tars/quay.io/mauilion tars/k8s.gcr.io tars/kindest
+echo "exporting images"
 
-#cat images | xargs -I {} docker image save {} -o tars/{}
-
+cat images | xargs -I {} docker image save {} -o tars/{}
+echo "building Dockerfile"
 echo "FROM bash" > Dockerfile
 echo "WORKDIR /images" >> Dockerfile
 echo "RUN apk add --update docker-cli && rm -rf /var/cache/apk/" >> Dockerfile
@@ -17,6 +17,7 @@ echo "COPY images /root/images" >> Dockerfile
 echo "COPY runit.sh /root/runit.sh" >> Dockerfile
 echo 'CMD ["/root/runit.sh"]' >> Dockerfile
 
-echo "building image"
-docker build -t quay.io/mauilion/kind-cache:latest .
-
+echo "building layered image"
+docker build -f Dockerfile -t quay.io/mauilion/kind-cache:unflat .
+container_id=$(docker run -d quay.io/mauilion/kind-cache:unflat /bin/true)
+docker export $container_id | docker import - quay.io/mauilion/kind-cache:latest
